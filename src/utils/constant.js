a1fb5ce126f64b9b938d634fe62d61a522ad7b9c
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-04-04 01:04:37 +0530

    changed link to HTTPS

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,1 @@
-export const WEATHER_API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=`;
+export const WEATHER_API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=`;
